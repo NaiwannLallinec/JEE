f2f3c3f1aa89b131ac164fcc0fe998cbee4a8080
ZitouneMcGregor <129374405+ZitouneMcGregor@users.noreply.github.com>
2023-10-31 17:29:54 +0100

    Add files via upload

---
+++ demoGr1/src/test/java/fr/cytech/pau/demoGr1/DemoGr1ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.cytech.pau.demoGr1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGr1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
